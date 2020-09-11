43782f69ee2cdce4768ba52a932b3dc3ba0cba69
fercc1097 <fercc1097@gmail.com>
2020-09-11 08:22:54 -0500

    this should fix the last bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,77 +1,75 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const webpack = require('webpack');
-const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin');
-const { CleanWebpackPlugin } = require ('clean-webpack-plugin');
-const TerserJSPlugin = require('terser-webpack-plugin');
-const optimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const webpack = require('webpack')
+const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const TersetJSPlugin = require('terser-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
-    entry: {
-        app:path.resolve(__dirname,'src','index.js'),
-},
-    output: {
-        path: path.resolve(__dirname,'docs'),
-        filename: `js/[name].[hash].js`,
-        publicPath: 'https://fercc1097.github.io/platzi-badges-challenge/',
-        chunkFilename: 'js/[id].[chunkhash].js'
-    },
-    optimization:{
-        minimizer: [
-            new TerserJSPlugin(),
-            new optimizeCSSAssetsPlugin()
+  entry: {
+    app: path.resolve(__dirname,'src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'docs'),
+    filename: 'js/[name].[hash].js',
+    publicPath: 'https://fercc1097.github.io/platzi-badges-challenge/',
+    chunkFilename: 'js/[id].[chunkhash].js'
+  },
+  optimization: {
+    minimizer: [
+      new TersetJSPlugin(),
+      new OptimizeCSSAssetsPlugin()
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
         ]
-    },
-
-    module:{
-        rules:[
-            {
-                test: /\.jpg|png|gif|woff|eot|ttf|svg|mp4|webm$/,
-                use: {
-                    loader: 'url-loader',
-                    options:{
-                        limit: 10000,
-                        name: '[name].[hash].[ext]',
-                        outputPath: '/assets'
-                    }
-                },
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.js$/i,
-                use: 'babel-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/i,
-                use: [
-                    {
-                        loader:MiniCssExtractPlugin.loader,
-                    },                    
-                    'css-loader'
-                    ],
-            }
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].[hash].css',
-            chunkFilename: 'css/[id].[chunkhash].css'
-        }),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname,'public/index.html')
-        }),
-        new webpack.DllReferencePlugin({
-            manifest: require('./modules-manifest.json')
-        }),
-        new AddAssetHtmlPlugin({
-            filepath: path.resolve(__dirname,'dist/js/*.dll.js'),
-            outputPath: 'js',
-            publicPath: 'https://fercc1097.github.io/platzi-badges-challenge/js'
-        }),
-        new CleanWebpackPlugin({
-            cleanOnceBeforeBuildPatterns: ['**/app.*'],
-        })
-    ],
+      },
+      {
+        test: /\.jpg|png|gif|woff|eot|ttf|svg|mp4|webm$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 1000,
+            name: '[hash].[ext]',
+            outputPath: 'assets'
+          }
+        }
+      },
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[hash].css',
+      chunkFilename: 'css/[id].[hash].css'
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html')
+    }),
+    new webpack.DllReferencePlugin({
+      manifest: require('./modules-manifest.json')
+    }),
+    new AddAssetHtmlPlugin({
+      filepath: path.resolve(__dirname, 'docs/js/*.dll.js'),
+      outputPath: 'js',
+      publicPath: 'https://fercc1097.github.io/platzi-badges-challenge/js'
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['**/app.*'],
+    })
+  ],
 }
